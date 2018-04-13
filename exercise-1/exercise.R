5f253fac9718277bf88ae3d4e08a0ce3b53f06e7
Mustafa <mustafa@D-173-250-160-17.dhcp4.washington.edu>
2018-04-13 11:05:55 -0700

    exercise 10 finised

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -26,27 +26,31 @@ length(unique(makes))
 
 # Filter the data set for vehicles manufactured in 1997
 
-vehicles_manufactured_in_1997 <- vehicles[vehicles$year == '1997',]
+cars_1997 <- vehicles[vehicles$year == '1997',]
 
 # Arrange the 1997 cars by highway (`hwy`) gas milage
 # Hint: use the `order()` function to get a vector of indices in order by value
 # See also:
 # https://www.r-bloggers.com/r-sorting-a-data-frame-by-the-contents-of-a-column/
 
-View(vehicles_manufactured_in_1997)
-cars_1997 <- vehicles_year[order(vehicles_year$hwy), ]
+cars_1997 <- cars_1997[order(cars_1997$hwy), ]
+
 # Mutate the 1997 cars data frame to add a column `average` that has the average
 # gas milage (between city and highway mpg) for each car
 
+cars_1997$average = (cars_1997$hwy + cars_1997$cty) / 2
 
 # Filter the whole vehicles data set for 2-Wheel Drive vehicles that get more
 # than 20 miles/gallon in the city. 
 # Save this new data frame in a variable.
 
+two_wheel_20_mpg <- vehicles[vehicles$drive == '2-Wheel Drive' & vehicles$cty > 20,]
 
 # Of the above vehicles, what is the vehicle ID of the vehicle with the worst 
 # hwy mpg?
 # Hint: filter for the worst vehicle, then select its ID.
+
+low_hwy_mpg <- two_wheel_20_mpg$id[two_wheel_20_mpg$hwy == min(two_wheel_20_mpg$hwy)]
 
 
 # Write a function that takes a `year_choice` and a `make_choice` as parameters, 
@@ -54,6 +58,11 @@ cars_1997 <- vehicles_year[order(vehicles_year$hwy), ]
 # of that make in that year.
 # You'll need to filter more (and do some selecting)!
 
+year_make_choice <- function(year_choice, make_choice) {
+  choice <- vehicles[vehicles$year == year_choice & vehicles$make == make_choice,]
+  choice[choice$hwy == max(choice$hwy), 'model']
+}
 
 # What was the most efficient Honda model of 1995?
 
+most_efficient_honda_1995 <- year_make_choice(1995, 'Honda')
