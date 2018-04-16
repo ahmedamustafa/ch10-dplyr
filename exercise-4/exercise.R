7da0bf1d44a0a51005bdf8ecd4efcdf0de48e2ec
Mustafa <mustafa@D-173-250-184-205.dhcp4.washington.edu>
2018-04-16 16:37:49 -0700

    half way done

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -3,22 +3,32 @@
 # Install the `nycflights13` package. Load (`library()`) the package.
 # You'll also need to load `dplyr`
 
+install.packages("dplyr")
+install.packages("nycflights13")
+library("nycflights13")
+library("dplyr")
 
 # The data frame `flights` should now be accessible to you.
 # Use functions to inspect it: how many rows and columns does it have?
 # What are the names of the columns?
 # Use `??flights` to search for documentation on the data set (for what the 
 # columns represent)
-
+dim(flights)
+colnames(flights)  
 
 # Use `dplyr` to give the data frame a new column that is the amount of time
 # gained or lost while flying (that is: how much of the delay arriving occured
 # during flight, as opposed to before departing).
 
+flights <- fights %>% 
+  mutate(time_gained - arr_delay - dep_delay)
+
 
 # Use `dplyr` to sort your data frame in descending order by the column you just
 # created. Remember to save this as a variable (or in the same one!)
 
+flights <- flights %>% 
+  arrange(-time_gained)
 
 # For practice, repeat the last 2 steps in a single statement using the pipe
 # operator. You can clear your environmental variables to "reset" the data frame
